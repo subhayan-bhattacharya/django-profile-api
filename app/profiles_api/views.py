b064077a473474b295aa1b2419ac0700297488a7
Subhayan <subhayan.aws@gmail.com>
2019-10-17 16:48:14 +0200

    Added functionality to create a login profile

--- app/profiles_api/views.py
+++ app/profiles_api/views.py
@@ -7,6 +7,8 @@ from .models import UserProfile
 from rest_framework.authentication import TokenAuthentication
 from .permissions import UpdateOwnProfile
 from rest_framework import filters
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
 
 class HelloApiView(APIView):
@@ -106,3 +108,8 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     permission_classes = (UpdateOwnProfile, )
     filter_backends = (filters.SearchFilter, )
     search_fields = ('name', 'email', )
+
+
+class UserLoginApiView(ObtainAuthToken):
+    """Handle creating user authentication tokens"""
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
